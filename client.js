eff42569fb60f228acd89757256cd4493af80e78
Himanshu Kumar Gola <himanshugola1111@gmail.com>
2024-02-25 23:35:29 +0530

    connect to redis-server ✅

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 import Redis from "ioredis";
 
-const client = new Redis();
+const client = new Redis("redis://default:380871ff6b3a4a96afc3a8bc43448e2a@apn1-credible-sawfish-34328.upstash.io:34328");
 
 export default client;
